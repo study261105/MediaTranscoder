aa3d698f08654dab9ad995d68d5533026bfda241
study261105 <65936964+study261105@users.noreply.github.com>
2020-08-06 23:33:42 +0800

    Add files via upload
    initial commit

---
+++ src/main/java/com/MediaTranscoder/MediaTranscoderApplication.java
@@ -0,0 +1,13 @@
+package com.MediaTranscoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTranscoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTranscoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MediaTranscoder/MediaTranscoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MediaTranscoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaTranscoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
